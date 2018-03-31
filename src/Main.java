0c3d98d4f0695db71b3ea8532bc9e86617c859e8
qumingyang <bksqmy@163.com>
2018-03-31 10:00:25 +0800

    提交内容修改

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+        List<String> strings = new ArrayList<String>();
+        strings.add("123");
+        for (String str:
+             strings) {
+            System.out.println(str);
+        }
     }
 }
